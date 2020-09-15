ad6eb6acf02e71e1e02f5068e3c3981b94dca077
Joel Ramos <ramosj3@wwu.edu>
2020-09-15 12:28:21 -0700

    added component skeleton and routing

---
+++ src/Shelf.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Shelf() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Shelf
---
+++ src/ShelfChanger.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShelfChanger() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ShelfChanger;
